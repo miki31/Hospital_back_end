02d0d52ff27dd08e54a74bff4bcccebd9c7a8b5f
miki31 <it.misha2016@gmail.com>
2019-11-14 02:06:22 +0200

    Test Controller :)

---
+++ src/main/java/com/test/hospital/test/TestController.java
@@ -0,0 +1,21 @@
+package com.test.hospital.test;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String sayHello() {
+        String datatime = LocalDateTime.now().format(DateTimeFormatter.ofPattern("dd - MMMM - yyyy -- HH:mm:ss"));
+        return "Rest Service is now available     " +
+                "IT`S WORKING. Now is : ___  " +
+                datatime;
+    }
+}
